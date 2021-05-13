509c972f4e6d05e2c76a3c90c6d5e88f4f89327b
nakamoriryota <ryota.nakamori@gmail.com>
2021-05-13 15:37:44 +0900

    文章追加

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener('keydown', (event) => {
-    console.log(event.key);
-  }); 
+
+  document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+      case 'ArrowUp': {
+        console.log('好きな文章')
+        break;
+      }
+  
+      case 'ArrowDown': {
+        console.log('好きな文章')
+        break;
+      }
+  
+      case 'ArrowRight': {
+        console.log('好きな文章')
+        break;
+      }
+  
+      case 'ArrowLeft': {
+        console.log('好きな文章')
+        break;
+      }
+    }
+  });
+  

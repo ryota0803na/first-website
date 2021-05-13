23256ee1bed421e1f1c96a2daaa0f65c8d664449
nakamoriryota <ryota.nakamori@gmail.com>
2021-05-13 15:40:55 +0900

    文章追加

--- src/index.js
+++ src/index.js
@@ -1,25 +1,33 @@
+document.addEventListener('keydown', (event) => {
+  switch (event.key) {
+    case 'ArrowUp': {
+      const button = document.getElementById('zero'); // DOM を取得する命令
 
-  document.addEventListener('keydown', (event) => {
-    switch (event.key) {
-      case 'ArrowUp': {
-        console.log('好きな文章')
-        break;
-      }
-  
-      case 'ArrowDown': {
-        console.log('好きな文章')
-        break;
-      }
-  
-      case 'ArrowRight': {
-        console.log('好きな文章')
-        break;
-      }
-  
-      case 'ArrowLeft': {
-        console.log('好きな文章')
-        break;
-      }
+      button.style.top = 0;
+      button.style.right = 0;
+
+      break;
     }
-  });
+
+    case 'ArrowDown': {
+      const button = document.getElementById('zero'); // DOM を取得する命令
+
+      button.style.top = 80;
+      button.style.right = 0;
+      
+      break;
+    }
+
+    case 'ArrowRight': {
+      console.log('右');
+      break;
+    }
+
+    case 'ArrowLeft': {
+      console.log('左');
+      break;
+    }
+  }
+});
+
   
